abbc48f0838e7f916433b559ab05416698d5d0c3
Martin Magr <mmagr@redhat.com>
2014-05-05 13:08:27 +0200

    Use absolute path for test_project.py

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
 
-export KANZO_PROJECT=./tests/test_project.py
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd "`dirname ${SCRIPT_PATH}`/.." > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+export KANZO_PROJECT="${SCRIPT_PATH}/tests/test_project.py"
 nosetests -vv
