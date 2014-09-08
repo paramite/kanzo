982dcc02fb743f812f43b543ae0dc13de00fed2d
Martin Mágr <mmagr@redhat.com>
2014-09-08 09:47:36 +0200

    Removed unnecessary cmds

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-pushd . > /dev/null
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 if ([ -h "${SCRIPT_PATH}" ]) then
   while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
 fi
 cd "`dirname ${SCRIPT_PATH}`/.." > /dev/null
 PROJECT_PATH=`pwd`;
-popd  > /dev/null
 
 export KANZO_PROJECT="${PROJECT_PATH}/tests/test_project.py"
 nosetests --verbosity=2 -w $PROJECT_PATH
